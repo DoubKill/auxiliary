ea0376b2855e36a24644a104b17b6c13d39c295d
unknown <914391033@qq.com>
2020-08-19 13:33:35 +0800

    工作站数据表创建及标注

---
+++ sync_work_station.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+"""
+auther: 
+datetime: 2020/8/19
+name: 
+"""
+import os
+import django
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
+django.setup()
+
+
+
+if __name__ == "__main__":
+    pass
