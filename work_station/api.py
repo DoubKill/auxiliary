cd7045364aac4f09320c4db4d19b3719b207b9c0
unknown <914391033@qq.com>
2020-08-19 14:43:59 +0800

    增加下发计划入中间表的公共方法

--- work_station/api.py
+++ work_station/api.py
@@ -5,32 +5,47 @@ auther:
 datetime: 2020/8/19
 name: 
 """
-import importlib
 
-from work_station import models as md
 from work_station import serializers as sz
-
-
-def _model_serializer(model):
-    return getattr(sz, f"{model.__name__}Serializer")
 
 
 class IssueWorkStation(object):
 
+    def __init__(self, model, data):
+        """
+        传入对应的模型类与数据
+        data 传入带id数据为修改中间表数据，
+             不传入则为新增
+        """
+        self.model = model
+        self.model_name = model.__name__
+        self.data = data
+
+    @property
+    def model_serializer(self):
+        """自动匹配序列化器"""
+        return getattr(sz, f"{self.model_name}Serializer")
+
+    def issue_to_db(self):
+        """
+        对接万隆中间表
+        将数据存入到中间表
+        """
+        id = self.data.get("id")
+        if id:
+            instance = self.model.objects.filter(id=id)
+            serializer = self.model_serializer(instance, data=self.data, partial="partial")
+        else:
+            serializer = self.model_serializer(data=self.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+    def issue_to_interface(self):
+        """对接国自组态"""
+        pass
+
+
+class UploadAuxiliary(object):
+
     def __init__(self):
         pass
-
-    def _model_name(self):
-        pass
-
-    def _model_serializer(self, model):
-        return getattr(sz, f"{model.__name__}Serializer")
-
-    def issue(self):
-        """
-        """
-        # mid_recipe = getattr(md, f"IfdownPmtRecipe{equip_no}")
-        # erializer = self._model_serializer()
-        pass
-        # if data.get("id"):
-
