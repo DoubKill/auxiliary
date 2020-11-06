da42355c47471e121a860ecec969dd91905f69ba
李威 <liwei@gzrobot.com>
2020-11-06 11:51:08 +0800

    [mod] 增加车次报表上传工厂日期

--- production/serializers.py
+++ production/serializers.py
@@ -90,6 +90,17 @@ class TrainsFeedbacksSerializer(BaseModelSerializer):
 class TrainsFeedbacksUpSerializer(BaseModelSerializer):
     """车次产出反馈上传"""
 
+    factory_date = serializers.SerializerMethodField(read_only=True)
+
+    def get_factory_date(self, object):
+        plan_uid = object.plan_classes_uid
+        pcp = ProductClassesPlan.objects.filter(plan_classes_uid=plan_uid).first()
+        if pcp:
+            date = pcp.work_schedule_plan.plan_schedule.day_time
+        else:
+            date = datetime.date.today()
+        return str(date)
+
     class Meta:
         model = TrainsFeedbacks
         fields = "__all__"
