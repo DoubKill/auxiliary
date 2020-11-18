979824c946907ec85379b70e7a51027042941870
afeng <1014676950@qq.com>
2020-11-18 09:11:55 +0800

    [mod]日料罐对接

--- mes/common_code.py
+++ mes/common_code.py
@@ -190,7 +190,7 @@ class SqlClient(object):
     def __init__(self, host=BZ_HOST, user=BZ_USR, password=BZ_PASSWORD, sql="select * from v_ASRS_STORE_MESVIEW",
                  db='GZSFJ', equip_no=None, equip_name=None):
         if equip_no and equip_name:
-            csi_obj = ChildSystemInfo.objects.filter(system_name=equip_name+equip_no).first()
+            csi_obj = ChildSystemInfo.objects.filter(system_name=equip_name + equip_no).first()
             host_actual = csi_obj.link_address
             user = "sa"
             password = "123"
