5db6dcc1bd37815cb2fd47e55462883c7c6cc40e
liwei <liwei@gzrobot.com>
2021-01-09 11:18:24 +0800

    [mod] 配置缓存

--- page_cache_crontab.py
+++ page_cache_crontab.py
@@ -6,4 +6,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
 django.setup()
 
 if __name__ == '__main__':
-    requests.get("http://127.0.0.1:8000/api/v1/production/equip-status-plan-list/")
+    requests.get("http://127.0.0.1:80/api/v1/production/equip-status-plan-list/")
