57a0bd79bc099a320d7833eeadcaecb60506598e
kevin <771411855@qq.com>
2021-07-29 19:38:27 +0800

    [mod]料框模型类新增字段

--- production/models.py
+++ production/models.py
@@ -438,6 +438,8 @@ class LoadMaterialLog(models.Model):
 
 
 class LoadTankMaterialLog(AbstractEntity):
+    plan_classes_uid = models.CharField(max_length=64, help_text='小料称量计划号')
+    scan_material = models.CharField(max_length=64, help_text='扫码物料名', default='')
     material_no = models.CharField(max_length=64, help_text='原材料编码')
     material_name = models.CharField(max_length=64, help_text='原材料名称')
     bra_code = models.CharField(max_length=64, help_text='条形码')
